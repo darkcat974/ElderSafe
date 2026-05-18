1317e7750981f2672a2c0aa4b2475d7d7ed33ffe
darkcat974 <fdajon974@gmail.com>
2026-05-19 02:32:00 +0800

    added docker + docker compose for deployement | use launch.sh at root of project to lauch

--- admin/my-dashboard/vite.config.js
+++ admin/my-dashboard/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Écoute sur 0.0.0.0 pour Docker
+    port: 5173
+  }
 })
